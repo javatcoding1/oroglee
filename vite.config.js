3b5001461f3c11f608f776768b5ad70cb092b6ff
javatcoding1 <jaminmadhav1>
2026-03-18 15:28:52 +0530

    updated the backend uri

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5001',
-        changeOrigin: true,
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      port: 5173,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL || 'http://localhost:5000', 
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
-})
+  })
+}
